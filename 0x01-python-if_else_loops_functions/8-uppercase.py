c5967de7439046ec54be8d86089fa1fdfcc685d1
EliasEshetuMamo <eliasmamo21+alx@gmail.com>
2022-11-29 18:27:08 +0000

    loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{}".format(i), end="")
+            i = chr(ord(i) - (ord('a') - ord('A')))
+    print("{}".format(i), end='')
     print()
