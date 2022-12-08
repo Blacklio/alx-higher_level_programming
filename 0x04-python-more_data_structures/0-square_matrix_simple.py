f57bc8c65cbea5eac4f4f36be36b942555d07342
EliasEshetuMamo <eliasmamo21+alx@gmail.com>
2022-12-08 04:10:49 +0000

    more data

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in (matrix):
-        new_matrix.append([x*x for x in row])
+            new_matrix.append([x*x for x in row])
     return new_matrix
